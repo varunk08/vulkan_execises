f9abf7686317d74c32350f70fa2af681c4c71323
k.varun kumar <varunk08@users.noreply.github.com>
2017-10-20 00:55:50 -0400

    vulkan initial commit

---
+++ testProject1/testProject1/main.cpp
@@ -0,0 +1,39 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
+
+int main()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* pWindow = glfwCreateWindow(800, 600, "vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extension supported" << std::endl;
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(pWindow))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(pWindow);
+
+    glfwTerminate();
+
+    return 0;
+}
