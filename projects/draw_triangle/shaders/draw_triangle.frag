2fe4efb1baadf4fbee6879345284e2f18b61c011
k.varun kumar <varunk08@users.noreply.github.com>
2018-02-03 10:37:24 -0500

    output texture coords as frag color

--- projects/draw_triangle/shaders/draw_triangle.frag
+++ projects/draw_triangle/shaders/draw_triangle.frag
@@ -1,10 +1,15 @@
 #version 450
 #extension GL_ARB_separate_shader_objects : enable
 
+layout (location=0) in vec3 fragColor;
+layout (location=1) in vec2 fragTexCoord;
+
 layout(location = 0) out vec4 outColor;
-layout(location=0) in vec3 fragColor;
 
 void main()
 {
-    outColor = vec4(fragColor, 1.0);
+    //outColor = vec4(fragColor, 1.0);
+
+    // output tex coord as frag color
+    outColor = vec4(fragTexCoord, 0.0, 1.0);
 }
--- projects/draw_triangle/src/HelloTriangleApp.cpp
+++ projects/draw_triangle/src/HelloTriangleApp.cpp
@@ -122,9 +122,10 @@ void HelloTriangleApp::InitVulkan()
     CreateFramebuffers();
     CreateCommandPool();
 
-	// texture image and view
+	// texture image, image view and sampler
 	CreateTextureImage();
 	CreateTextureImageView();
+	CreateTextureSampler();
 	
 	// buffers
 	CreateVertexBuffer();
@@ -1701,22 +1702,22 @@ VkImageView HelloTriangleApp::CreateImageView(VkImage image, VkFormat format)
 void HelloTriangleApp::CreateTextureSampler()
 {
 	VkSamplerCreateInfo samplerInfo = {};
-	samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
-	samplerInfo.magFilter = VK_FILTER_LINEAR;
-	samplerInfo.minFilter = VK_FILTER_LINEAR;
-	samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-	samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-	samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-	samplerInfo.anisotropyEnable = VK_TRUE;
-	samplerInfo.maxAnisotropy = 16;
-	samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+	samplerInfo.sType				    = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+	samplerInfo.magFilter				= VK_FILTER_LINEAR;
+	samplerInfo.minFilter				= VK_FILTER_LINEAR;
+	samplerInfo.addressModeU			= VK_SAMPLER_ADDRESS_MODE_REPEAT;
+	samplerInfo.addressModeV			= VK_SAMPLER_ADDRESS_MODE_REPEAT;
+	samplerInfo.addressModeW			= VK_SAMPLER_ADDRESS_MODE_REPEAT;
+	samplerInfo.anisotropyEnable		= VK_TRUE;
+	samplerInfo.maxAnisotropy			= 16;
+	samplerInfo.borderColor				= VK_BORDER_COLOR_INT_OPAQUE_BLACK;
 	samplerInfo.unnormalizedCoordinates = VK_FALSE; // which addressing mode to use
-	samplerInfo.compareEnable = VK_FALSE;
-	samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
-	samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
-	samplerInfo.mipLodBias = 0.0f;
-	samplerInfo.minLod = 0.0f;
-	samplerInfo.maxLod = 0.0f;
+	samplerInfo.compareEnable			= VK_FALSE;
+	samplerInfo.compareOp				= VK_COMPARE_OP_ALWAYS;
+	samplerInfo.mipmapMode				= VK_SAMPLER_MIPMAP_MODE_LINEAR;
+	samplerInfo.mipLodBias				= 0.0f;
+	samplerInfo.minLod				    = 0.0f;
+	samplerInfo.maxLod					= 0.0f;
 
 	VkResult result = vkCreateSampler(m_device, &samplerInfo, nullptr, &m_textureSampler);
 
